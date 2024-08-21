8852069ea60874a96f99d723d7496c4e0d5133ba
Yunus Emre Dunay <ydunay@k2m15s01.42kocaeli.com.tr>
2024-08-21 16:24:38 +0300

    a

---
+++ CPP Module 00 /ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if(ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for(int i = 1; i < ac; i++)
+            for (int j = 0; av[i][j] != 0 ; j++)
+                av[i][j] >= 'a' && av[i][j] <= 'z' ? std::cout << (char)std::toupper(av[i][j]) : std::cout << av[i][j];
+        std::cout << std::endl;
+    }
+}
